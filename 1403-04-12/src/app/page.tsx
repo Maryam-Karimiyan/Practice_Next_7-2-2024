4526d52cd969bcbab2b73fe76e50ccaa33429f22
maryam <Ofogh.maryam14@gmail.com>
2024-07-05 16:07:46 +0330

    ex6

--- 1403-04-12/src/app/page.tsx
+++ 1403-04-12/src/app/page.tsx
@@ -1,15 +1,27 @@
 import Image from "next/image";
-import laserImg from "../../public/lazar.png"
+import laserImg from "../../public/lazar.png";
 import styles from "./page.module.css";
 import Link from "next/link";
+import Head from "next/head";
 export default function Home() {
   return (
-    <main className={styles.main}>
-     <Link href="/about">Go To About Page</Link>
-     <Image src={laserImg} alt="laser"/>
-     <Image src="https://abangraphic.ir/wp-content/uploads/2021/10/48005.jpg"
-     alt="dragon"
-     width={300} height={300}/>
-    </main>
+    <>
+      <Head>
+        <title>Homepage</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="og:title" content="My awesome course" key="title" />
+        <meta name="og:title" content="My Next.js course" key="title" /> 
+      </Head>
+      <main className={styles.main}>
+        <Link href="/about">Go To About Page</Link>
+        <Image src={laserImg} alt="laser" />
+        <Image
+          src="https://abangraphic.ir/wp-content/uploads/2021/10/48005.jpg"
+          alt="dragon"
+          width={300}
+          height={300}
+        />
+      </main>
+    </>
   );
 }
