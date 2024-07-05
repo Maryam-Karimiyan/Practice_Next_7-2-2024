a1e12f32253775a285a70f7f3dcaaa118a94f87a
maryam <Ofogh.maryam14@gmail.com>
2024-07-05 16:00:58 +0330

    ex5

--- 1403-04-12/src/app/page.tsx
+++ 1403-04-12/src/app/page.tsx
@@ -6,7 +6,10 @@ export default function Home() {
   return (
     <main className={styles.main}>
      <Link href="/about">Go To About Page</Link>
-     <Image src={laserImg} alt="laser" width={300} height={300}/>
+     <Image src={laserImg} alt="laser"/>
+     <Image src="https://abangraphic.ir/wp-content/uploads/2021/10/48005.jpg"
+     alt="dragon"
+     width={300} height={300}/>
     </main>
   );
 }
