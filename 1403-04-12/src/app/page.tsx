07fe2a0f9cf5406b883e0baa12e2c330d839e354
maryam <Ofogh.maryam14@gmail.com>
2024-07-05 14:59:34 +0330

    ex4

--- 1403-04-12/src/app/page.tsx
+++ 1403-04-12/src/app/page.tsx
@@ -1,10 +1,11 @@
-// import Image from "next/image";
+import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
 export default function Home() {
   return (
     <main className={styles.main}>
      <Link href="/about">Go To About Page</Link>
+     <Image src="/laser.png" alt="laser" width={100} height={100}/>
     </main>
   );
 }
